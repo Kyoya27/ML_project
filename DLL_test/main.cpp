e2df7e4ba041411e3143c0118c5869e4fe69ece0
CapelYohann <ieauhann@gmail.com>
2020-05-11 20:06:41 +0200

    cpp

---
+++ DLL_test/main.cpp
@@ -0,0 +1,87 @@
+#if _WIN32
+#define DLLEXPORT __declspec(dllexport)
+#else
+#define DLLEXPORT
+#endif
+
+#include <iostream>
+
+extern "C" {
+	DLLEXPORT double* linear_model_create(int input_dim);
+	DLLEXPORT double linear_model_predict_classification(double* model, double* inputs, int inputs_size);
+	DLLEXPORT void linear_model_train_classification(
+		double* model,
+		double** dataset_inputs,
+		int dataset_length,
+		int inputs_size,
+		double* dataset_expected_outputs,
+		int outputs_size,
+		int iterations_count,
+		float alpha
+	);
+	DLLEXPORT void linear_model_train_regression(
+		double* model,
+		double** dataset_inputs,
+		int dataset_length,
+		int inputs_size,
+		double* dataset_expected_outputs,
+		int outputs_size,
+		int iterations_count,
+		float alpha
+	);
+}
+
+int main() {
+	double blue_points[1][2] = {
+		{0.35, 0.5}
+	};
+
+	double red_points[2][2] = {
+		{0.6, 0.6},
+		{0.55, 0.7}
+	};
+
+	double X[3][3] = {
+		{1, 0.35, 0.5},
+		{1, 0.6, 0.6},
+		{1, 0.55, 0.7}
+	};
+	//double inputs[3][2]{
+	//	{0.35, 0.5 },
+	//	{0.6, 0.6 },
+	//	{0.55, 0.7 }
+	//};
+
+	double** inputs = new double*[3];
+	for (int i = 0; i < 3; i++) {
+		inputs[i] = new double[2];
+	}
+	inputs[0][0] = 0.35;
+	inputs[0][1] = 0.5;
+	inputs[1][0] = 0.6;
+	inputs[1][1] = 0.6;
+	inputs[2][0] = 0.55;
+	inputs[2][1] = 0.7;
+
+
+
+	double Y[3] = { 1, -1, -1 };
+
+	double* model = linear_model_create(3);
+	std::cout << linear_model_predict_classification(model, inputs[0], 2);
+	std::cout << linear_model_predict_classification(model, inputs[1], 2);
+	std::cout << linear_model_predict_classification(model, inputs[2], 2);
+
+	linear_model_train_classification(model, inputs, 3, 3, Y, 3, 1000, 0.01);
+
+	std::cout << linear_model_predict_classification(model, inputs[0], 2);
+	std::cout << linear_model_predict_classification(model, inputs[1], 2);
+	std::cout << linear_model_predict_classification(model, inputs[2], 2);
+	std::cout << std::endl;
+
+	for (int i = 0; i < 3; i++) {
+		std::cout << model[i] << " ";
+	}
+	
+	return 0;
+}
--- ML_project/Source.cpp
+++ ML_project/Source.cpp
@@ -4,11 +4,70 @@
 #define DLLEXPORT
 #endif
 
+#include <cmath>
+#include <time.h>
+#include <iostream>
+#include <Eigen/Core>
+
+using Eigen::Matrix2d;
+
+
 extern "C" {
-	DLLEXPORT int my_add(int x, int y) {
-		return x + y;
-	}
-	DLLEXPORT int my_mul(int x, int y) {
-		return x * y;
-	}
+    DLLEXPORT double* linear_model_create(int input_dim) {
+        srand(time(NULL));
+        double* array = new double[input_dim];
+        
+        for (int i = 0; i < input_dim; i++) {
+            double rd = (double)(rand() / RAND_MAX);
+            array[i] = rd;
+        }
+
+        return array;
+    }
+
+    DLLEXPORT double linear_model_predict_regression(double* model, double* inputs, int inputs_size) {
+        double res = 0;
+
+        for (int i = 0; i < inputs_size; i++) {
+            res += model[i+1] * inputs[i];
+        }
+
+        return res + model[0];
+    }
+
+    DLLEXPORT double linear_model_predict_classification(double* model, double* inputs, int inputs_size) {
+        return linear_model_predict_regression(model, inputs, inputs_size) >= 0 ? 1.0 : -1.0;
+    }
+
+    DLLEXPORT void linear_model_train_classification(double* model, double** dataset_inputs, int dataset_length, int inputs_size, double* dataset_expected_outputs, int outputs_size, int iterations_count, float alpha) {
+        //rosen
+        
+        for (int i = 0; i < iterations_count; i++) {
+            srand(time(NULL));
+            int k = rand() % inputs_size;
+            double g_x_k = linear_model_predict_classification(model, dataset_inputs[k], inputs_size);
+            double grad = alpha * (dataset_expected_outputs[k] - g_x_k);
+            model[0] += grad * 1;
+            for (int j = 0; j < inputs_size; j++) {
+                model[j + 1] += grad * dataset_inputs[k][j];
+            }
+        }
+    }
+
+    DLLEXPORT void linear_model_train_regression(double* model, double** dataset_inputs, int dataset_length, int inputs_size, double dataset_expected_outputs, int outputs_size, int iterations_count, float alpha) {
+        //PseudoInverse
+        double b0 = 0;
+        double b1 = 0;
+
+        for (int i = 0; i < iterations_count * inputs_size; i++) {
+            int idx = i % inputs_size;
+            double p = b0 + b1 * dataset_inputs[idx][0];
+            double err = p - dataset_inputs[idx][1];
+            b0 = b0 - alpha * err;
+            b1 = b1 - alpha * err * dataset_inputs[idx][0];
+            model[0] = b1;
+            model[1] = b0;
+            model[2] = err;
+        }
+    }
 }
